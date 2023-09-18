f80dc444881e4533244c4307b2cf3ffae8e82568
Vitaly <vitalydude83@gmail.com>
2023-09-18 12:11:26 +0300

    for_while

---
+++ for_while.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    //int a = 0;
+
+    for (int i = 0; i < 5; i++) {
+        printf("for: %d\n", i);
+        int b = 0;
+        while (b < 5) {
+            printf("while: %d\n", i);
+            b++;
+            int c = 0;
+            do {
+                printf("do_while: %d\n", i);
+                c++;
+            }
+            while (c < 5);
+        }
+    }
+    return 0;
+
+}
