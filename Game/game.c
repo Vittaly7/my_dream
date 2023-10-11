024473c75d3b28ba2731d4784fcc8c0d4d6b7e55
Vitaly <vitalydude83@gmail.com>
2023-10-11 09:00:40 +0300

    Game

--- Game/game.c
+++ Game/game.c
@@ -16,22 +16,3 @@ int main (){
     }
     return 0;
 }
-/*#include <stdio.h>
-
-int main() {
-    int width = 20;
-    int height = 10;
-
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
-                printf("#");
-            } else {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
-
-    return 0;
-}*/
