522dc99c2b4cc90f1ce99b8ae6ee33d7a048cde0
Vitaly <vitalydude83@gmail.com>
2023-10-15 14:27:28 +0300

    game2.0

--- Game/game.c
+++ Game/game.c
@@ -4,14 +4,27 @@ int main (){
 
     int width = 20;
     int hight = 10;
-    for(int i = 0; i < hight; i++){
+    int widthX = 0;
+    int hightY = 0;
+    printf("Enter the first coordinates X, Y: ");
+    scanf(" %d %d", &widthX, &hightY);
+    //if(y == 0 || y == hight - 1 || x == 0 || x == width - 1)
+    //    printf("#");
+    // else
+    //   printf("ERROR\n");
+        for(int i = 0; i < hight; i++){ // grid and initial symbol position
                 
-        for(int q = 0; q < width; q++){
-            if(i == 0 || i == hight - 1 || q == 0 || q == width - 1 || i == 5 && q == 10)
+            for(int q = 0; q < width; q++){
+                if(i == 0 || i == hight - 1 || q == 0 || q == width - 1){
                 printf("#");
-            else
+                }
+                else if(i == hightY && q == widthX){
+                printf("#");
+                }
+                else
                 printf(" ");
-        }
+            }
+        
         printf("\n");
     }
     return 0;
